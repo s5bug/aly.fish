eeeb44453c50c7f60d8e146196c2026ac276a800
Aly <me@s5.pm>
2025-06-13 05:08:56 -0700

    Add cache to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,22 @@
 import { defineMiddleware, sequence } from "astro:middleware";
-import {getRelativeLocaleUrl, getAbsoluteLocaleUrl, redirectToDefaultLocale, middleware} from 'astro:i18n';
-import {i18n} from 'astro:config/server';
+import {
+  getRelativeLocaleUrl,
+  getAbsoluteLocaleUrl,
+  redirectToDefaultLocale,
+  middleware,
+} from "astro:i18n";
+import { i18n } from "astro:config/server";
 
-const apiContextRoutesSymbol = Symbol.for('context.routes');
+const apiContextRoutesSymbol = Symbol.for("context.routes");
+
+const extantPagesSet: Set<string> = new Set();
+const missingPagesSet: Set<string> = new Set();
 
 export const userMiddleware = defineMiddleware(async (ctx, next) => {
-  if(ctx.locals.alyfish_middleware_nest || ctx.request.method === "HEAD") return next();
+  if (ctx.locals.alyfish_middleware_nest) return await next();
   else ctx.locals.alyfish_middleware_nest = false;
 
-  const locales = i18n!.locales as string[]
+  const locales = i18n!.locales as string[];
 
   let ourLocale: string | undefined;
   for (const locale of locales) {
@@ -18,71 +26,91 @@ export const userMiddleware = defineMiddleware(async (ctx, next) => {
     }
   }
 
-  if(ourLocale) {
-    ctx.locals.alyfish_here_locale = ourLocale
+  if (ourLocale) {
+    ctx.locals.alyfish_here_locale = ourLocale;
   } else {
-    ctx.locals.alyfish_here_locale = undefined
+    ctx.locals.alyfish_here_locale = undefined;
   }
 
   let stripLocaleUrl: string;
-  if(!ourLocale) stripLocaleUrl = ctx.url.pathname;
+  if (!ourLocale) stripLocaleUrl = ctx.url.pathname;
   else stripLocaleUrl = ctx.url.pathname.substring(2 + ourLocale.length);
   ctx.locals.alyfish_here_url_stripped = stripLocaleUrl;
 
-  ctx.locals.alyfish_here_alternates = []
+  ctx.locals.alyfish_here_alternates = [];
+
+  if (ctx.request.method === "HEAD") return await next();
 
   // When we rewrite to another page, we don't want this middleware applying to it
   // All rendering operations happen independently, so we won't cache the result of rendering without this middleware
   ctx.locals.alyfish_middleware_nest = true;
-  for(const locale of locales) {
-    if(locale === ourLocale) continue;
+  for (const locale of locales) {
+    if (locale === ourLocale) continue;
+
+    const relativeUrl = getRelativeLocaleUrl(locale, stripLocaleUrl);
+    if (extantPagesSet.has(relativeUrl)) {
+      ctx.locals.alyfish_here_alternates.push(locale);
+      continue;
+    }
+    if (missingPagesSet.has(relativeUrl)) continue;
+
     let exists = false;
     try {
-      const relativeUrl = getRelativeLocaleUrl(locale, stripLocaleUrl);
-      if(ctx.isPrerendered) {
+      if (ctx.isPrerendered) {
         // Introducing `any` here because I don't believe I have access to Pipeline type
         const pipeline: any = Reflect.get(ctx, apiContextRoutesSymbol);
-        const {routeData} = await pipeline.tryRewrite(
+        const { routeData } = await pipeline.tryRewrite(
           relativeUrl,
           ctx.request,
         );
         if (routeData.prerender) exists = true;
       } else {
-        const result = await fetch(new URL(relativeUrl, ctx.url), { method: "HEAD" });
-        if(result.ok) exists = true;
+        const result = await fetch(new URL(relativeUrl, ctx.url), {
+          method: "HEAD",
+        });
+        if (result.ok) exists = true;
       }
     } catch (e) {
       // ignore
     }
-    if(exists) {
-      ctx.locals.alyfish_here_alternates.push(locale)
+    if (exists) {
+      extantPagesSet.add(relativeUrl);
+      ctx.locals.alyfish_here_alternates.push(locale);
+    } else {
+      missingPagesSet.add(relativeUrl);
     }
   }
   ctx.locals.alyfish_middleware_nest = false;
 
   // Then if we're a pre-rendered page, we don't want to reply with a redirect
-  if(ctx.isPrerendered) return await next();
+  if (ctx.isPrerendered) return await next();
 
-  if(ctx.preferredLocaleList) {
-    for(const preferredLocale of ctx.preferredLocaleList) {
-      if(ctx.locals.alyfish_here_alternates.includes(preferredLocale)) {
-        return ctx.redirect(getRelativeLocaleUrl(preferredLocale, stripLocaleUrl), 302)
+  if (ctx.preferredLocaleList) {
+    for (const preferredLocale of ctx.preferredLocaleList) {
+      if (ctx.locals.alyfish_here_alternates.includes(preferredLocale)) {
+        return ctx.redirect(
+          getRelativeLocaleUrl(preferredLocale, stripLocaleUrl),
+          302,
+        );
       }
     }
   }
 
-  if(ctx.locals.alyfish_here_alternates.includes(i18n!.defaultLocale)) {
-    return ctx.redirect(getRelativeLocaleUrl(i18n!.defaultLocale, stripLocaleUrl), 302)
+  if (ctx.locals.alyfish_here_alternates.includes(i18n!.defaultLocale)) {
+    return ctx.redirect(
+      getRelativeLocaleUrl(i18n!.defaultLocale, stripLocaleUrl),
+      302,
+    );
   }
 
   return await next();
-})
+});
 
 export const onRequest = sequence(
   userMiddleware,
   middleware({
     redirectToDefaultLocale: false,
     prefixDefaultLocale: true,
-    fallbackType: "redirect"
-  })
-)
+    fallbackType: "redirect",
+  }),
+);
